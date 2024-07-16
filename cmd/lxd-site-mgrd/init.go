8de325154622b433f3a9eda8b9a3545d003eb62e
Mason Hu <mason.hu@canonical.com>
2024-07-16 14:38:04 +0200

    state: add global state for site manager
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- cmd/lxd-site-mgrd/init.go
+++ cmd/lxd-site-mgrd/init.go
@@ -7,16 +7,17 @@ import (
 
 	"github.com/canonical/microcluster/microcluster"
 	"github.com/canonical/microcluster/rest/types"
-	"github.com/canonical/microcluster/state"
+	microState "github.com/canonical/microcluster/state"
 
 	"github.com/canonical/lxd-site-manager/internal/api"
 	"github.com/canonical/lxd-site-manager/internal/database"
+	"github.com/canonical/lxd-site-manager/internal/state"
 )
 
 // InitialiseControlListener is a hook that initialises the control listener for the daemon.
 // It will update the local daemon server configs with the control listener address and start the control listener.
 // It will also create a member config entry in dqlite with he control listener address.
-func InitialiseControlListener(ctx context.Context, s state.State, m *microcluster.MicroCluster, initConfig map[string]string) error {
+func InitialiseControlListener(ctx context.Context, clusterState microState.State, m *microcluster.MicroCluster, initConfig map[string]string) error {
 	controlListenerAddress, ok := initConfig[string(api.ControlListener)]
 	if !ok {
 		return fmt.Errorf("control listener address not provided")
@@ -38,14 +39,32 @@ func InitialiseControlListener(ctx context.Context, s state.State, m *microclust
 		return fmt.Errorf("failed to update initialise control listener: %w", err)
 	}
 
-	return s.Database().Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
+	return clusterState.Database().Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
 		var err error
 		_, err = database.CreateManagerMemberConfig(ctx, tx, database.ManagerMemberConfig{
-			Target:          s.Name(),
+			Target:          clusterState.Name(),
 			HTTPSAddress:    controlListenerAddress,
 			ExternalAddress: "",
 		})
 
+		return err
+	})
+}
+
+// InitialiseConfigOIDC is a hook that initialises the OIDC config for the daemon.
+func InitialiseConfigOIDC(ctx context.Context, clusterState microState.State, siteManagerState *state.SiteManagerState) error {
+	return clusterState.Database().Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
+		dbConfigs, err := database.GetManagerConfig(ctx, tx)
+		if err != nil {
+			return err
+		}
+
+		configs := make(map[string]string)
+		for _, c := range dbConfigs {
+			configs[c.Key] = c.Value
+		}
+
+		err = api.UpdateDaemonOIDCConfig(configs, siteManagerState, clusterState)
 		return err
 	})
 }
