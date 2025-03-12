44f3217dd3dc284067c4168d81e2b12686ab7a05
David Edler <david.edler@canonical.com>
2025-03-12 13:36:20 +0100

    chore(ui): adjust revoke token button layout
    Signed-off-by: David Edler <david.edler@canonical.com>

--- ui/src/pages/clusters/RevokeTokenButton.tsx
+++ ui/src/pages/clusters/RevokeTokenButton.tsx
@@ -1,61 +1,62 @@
-import { FC, useState } from "react";
-import { useQueryClient } from "@tanstack/react-query";
-import { ConfirmationButton, useNotify } from "@canonical/react-components";
-import { deleteToken } from "api/tokens";
-import { Token } from "types/token";
-import { queryKeys } from "util/queryKeys";
+import {FC, useState} from "react";
+import {useQueryClient} from "@tanstack/react-query";
+import {ConfirmationButton, Icon, useNotify} from "@canonical/react-components";
+import {deleteToken} from "api/tokens";
+import {Token} from "types/token";
+import {queryKeys} from "util/queryKeys";
 
 interface Props {
-  token: Token;
+    token: Token;
 }
 
-const RevokeTokenButton: FC<Props> = ({ token }) => {
-  const queryClient = useQueryClient();
-  const notify = useNotify();
-  const [loading, setLoading] = useState(false);
+const RevokeTokenButton: FC<Props> = ({token}) => {
+    const queryClient = useQueryClient();
+    const notify = useNotify();
+    const [loading, setLoading] = useState(false);
 
-  const handleDeleteToken = async () => {
-    setLoading(true);
-    await deleteToken(token.cluster_name)
-      .then(() => {
-        void queryClient.invalidateQueries({
-          queryKey: [queryKeys.tokens],
-        });
-        notify.success(
-          `Successfully revoked token for cluster ${token.cluster_name}.`,
-        );
-      })
-      .catch((e: Error) => {
-        notify.failure(`Unable to revoke token ${token.cluster_name}.`, e);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+    const handleDeleteToken = async () => {
+        setLoading(true);
+        await deleteToken(token.cluster_name)
+            .then(() => {
+                void queryClient.invalidateQueries({
+                    queryKey: [queryKeys.tokens],
+                });
+                notify.success(
+                    `Successfully revoked token for cluster ${token.cluster_name}.`,
+                );
+            })
+            .catch((e: Error) => {
+                notify.failure(`Unable to revoke token ${token.cluster_name}.`, e);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
 
-  return (
-    <ConfirmationButton
-      appearance="negative"
-      loading={loading}
-      className="u-no-margin--bottom"
-      confirmationModalProps={{
-        title: "Confirm revoke",
-        children: (
-          <p>
-            This will permanently revoke the token for cluster{" "}
-            <strong>{token.cluster_name}</strong>. This action cannot be undone,
-            and can result in data loss.
-          </p>
-        ),
-        confirmButtonLabel: "Revoke",
-        onConfirm: () => void handleDeleteToken(),
-      }}
-      shiftClickEnabled
-      showShiftClickHint
-    >
-      Revoke
-    </ConfirmationButton>
-  );
+    return (
+        <ConfirmationButton
+            appearance=""
+            loading={loading}
+            className="u-no-margin--bottom has-icon"
+            confirmationModalProps={{
+                title: "Confirm revoke",
+                children: (
+                    <p>
+                        This will permanently revoke the token for cluster{" "}
+                        <strong>{token.cluster_name}</strong>. This action cannot be undone,
+                        and can result in data loss.
+                    </p>
+                ),
+                confirmButtonLabel: "Revoke",
+                onConfirm: () => void handleDeleteToken(),
+            }}
+            shiftClickEnabled
+            showShiftClickHint
+        >
+            <Icon name="delete"/>
+            <span>Revoke</span>
+        </ConfirmationButton>
+    );
 };
 
 export default RevokeTokenButton;
